1847ec1c9e5bf42a809528a45cfebdebba5e893b
andiwand <stefl.andreas@gmail.com>
2022-12-17 10:41:01 +0100

    simplify day 16

--- 2022/day16/solution.py
+++ 2022/day16/solution.py
@@ -9,52 +9,59 @@ for l in open(Path(__file__).resolve().parent / 'input.txt'):
   valves[f] = int(p)
   tunnels[f] = tuple(t.split(', '))
 
+def shortest_paths(start, tunnels):
+  result = {start: 0}
+  queue = [start]
+  while queue:
+    c = queue.pop()
+    for n in tunnels[c]:
+      if n not in result or result[c]+1<result[n]:
+        result[n]=result[c]+1
+        queue.append(n)
+  return result
+
+working_valves = set(v for v in valves if valves[v] > 0)
+shortest = {v:shortest_paths(v,tunnels) for v in ['AA'] + list(working_valves)}
+
 def pressure(open_valves):
   return sum(valves[s] for s in open_valves)
 
-def solve(valves, working_valves, tunnels, start, minutes, open_valves, dp, path):
+def solve(start, minutes, available_valves, dp):
   if minutes == 0:
-    return (0, path)
+    return 0
+  if len(available_valves) == 0:
+    return 0
 
-  fingerprint = (start, minutes, open_valves)
+  fingerprint = (start, minutes, available_valves)
 
   if fingerprint in dp:
     return dp[fingerprint]
 
-  current = pressure(open_valves)
+  options = [0]
 
-  if len(open_valves) == working_valves:
-    dp[fingerprint] = (current*minutes, path)
-    return dp[fingerprint]
+  for v in available_valves:
+    d = shortest[start][v]
+    if d+1>minutes:
+      continue
+    s = solve(v, minutes-d-1, frozenset(available_valves-set([v])), dp)
+    options.append(s+valves[v]*(minutes-d-1))
 
-  options = []
-
-  if valves[start] > 0 and start not in open_valves:
-    next_open = frozenset(open_valves | set([start]))
-    options.append(solve(valves, working_valves, tunnels, start, minutes-1, next_open, dp, [f'open {start}'] if path is not None else None))
-
-  for n in tunnels[start]:
-    options.append(solve(valves, working_valves, tunnels, n, minutes-1, open_valves, dp, [f'goto {n}'] if path is not None else None))
-
-  best = max(options, key=lambda x: x[0])
-  dp[fingerprint] = (best[0]+current, path+best[1] if path is not None else None)
+  best = max(options)
+  dp[fingerprint] = best
   return dp[fingerprint]
 
-working_valves = sum(1 for _,p in valves.items() if p > 0)
-
 dp = {}
-s = solve(valves, working_valves, tunnels, 'AA', 30, frozenset(), dp, ['start AA'])
+s = solve('AA', 30, frozenset(working_valves), dp)
 print(s, len(dp))
 
 import itertools
 
 s = 0
-for i in range(working_valves//2-4, working_valves//2+1):
-  for new_valves in itertools.combinations([v for v in valves if valves[v] > 0], i):
-    new_valves1 = {v:0 for v in valves} | {v:valves[v] for v in new_valves}
-    new_valves2 = {v:0 for v in valves} | {v:valves[v] for v in set(valves)-set(new_valves)}
-    a = solve(new_valves1, i, tunnels, 'AA', 26, frozenset(), {}, None)
-    b = solve(new_valves2, working_valves-i, tunnels, 'AA', 26, frozenset(), {}, None)
-    if a[0]+b[0] > s:
-      s = a[0]+b[0]
+for i in range(0, len(working_valves)//2+1):
+  for available_valves1 in itertools.combinations(working_valves, i):
+    available_valves1 = frozenset(available_valves1)
+    s1 = solve('AA', 26, available_valves1, dp)
+    s2 = solve('AA', 26, frozenset(working_valves - available_valves1), dp)
+    if s1+s2 > s:
+      s = s1+s2
 print(s)
