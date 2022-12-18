c8f93736133569374bc0e2d0624f0dd29a5e3d2e
andiwand <stefl.andreas@gmail.com>
2022-12-18 12:32:33 +0100

    refactor nd

--- 2022/day18/solution.py
+++ 2022/day18/solution.py
@@ -1,13 +1,16 @@
 from pathlib import Path
+import itertools
 
 droplet = frozenset(tuple(map(int, l.strip().split(','))) for l in open(Path(__file__).resolve().parent / 'input.txt'))
 
 def next_neighbours(p):
-  return frozenset(
-    [(p[0]+i,p[1],p[2]) for i in [-1,1]] +
-    [(p[0],p[1]+i,p[2]) for i in [-1,1]] +
-    [(p[0],p[1],p[2]+i) for i in [-1,1]]
-  )
+  result = []
+  for i in range(len(p)):
+    result.extend([
+      tuple(p[j] if j!=i else p[j]-1 for j in range(len(p))),
+      tuple(p[j] if j!=i else p[j]+1 for j in range(len(p)))
+    ])
+  return frozenset(result)
 
 def surface(o):
   touching = sum(len(o & next_neighbours(p)) for p in o)
@@ -16,34 +19,29 @@ def surface(o):
 print(surface(droplet))
 
 def dimensions(o):
-  return (
-    (min(x for x,_,_ in o), max(x for x,_,_ in o)),
-    (min(y for _,y,_ in o), max(y for _,y,_ in o)),
-    (min(z for _,_,z in o), max(z for _,_,z in o))
-  )
+  return tuple((min(p[i] for p in o), max(p[i] for p in o)) for i in range(len(next(iter(o)))))
+
+def inside(p, d):
+  return all(d[i][0]<=p[i]<=d[i][1] for i in range(len(p)))
 
 def invert(o, d):
   result = set()
-  for x in range(d[0][0], d[0][1]+1):
-    for y in range(d[1][0], d[1][1]+1):
-      for z in range(d[2][0], d[2][1]+1):
-        if (x,y,z) not in o:
-          result.add((x,y,z))
+  for p in itertools.product(*[range(r[0],r[1]+1) for r in d]):
+    if p not in o:
+      result.add(p)
   return frozenset(result)
 
 def submerge(o, d):
+  d = tuple((r[0]-1,r[1]+1) for r in d)
   result = set()
-  queue = [(d[0][0]-1,d[1][0]-1,d[2][0]-1)]
+  queue = [tuple(r[0] for r in d)]
   while queue:
     c = queue.pop()
     for n in next_neighbours(c):
-      if n not in result and n not in o and (
-        d[0][0]-1<=n[0]<=d[0][1]+1 and
-        d[1][0]-1<=n[1]<=d[1][1]+1 and
-        d[2][0]-1<=n[2]<=d[2][1]+1):
+      if n not in result and n not in o and inside(n, d):
         queue.append(n)
         result.add(n)
-  return result
+  return frozenset(result)
 
 droplet_dim = dimensions(droplet)
 droplet_outside = submerge(droplet, droplet_dim)
