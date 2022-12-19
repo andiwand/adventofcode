0e1d1ad280e42fc2d807684558b3e0b1966ddb66
andiwand <stefl.andreas@gmail.com>
2022-12-19 14:24:23 +0100

    day 19 refactor after hack

--- 2022/day19/solution.py
+++ 2022/day19/solution.py
@@ -8,7 +8,7 @@ for l in open(Path(__file__).resolve().parent / 'input.txt'):
   g = tuple(map(int, m.groups()))
   blueprints.append(((g[1],0,0,0), (g[2],0,0,0), (g[3],g[4],0,0), (g[5],0,g[6],0)))
 
-def sim(blueprint, robots, material, minutes, mem):
+def sim(blueprint, robots, material, minutes, mem, build_skipper):
   if minutes <= 0:
     return material
 
@@ -28,9 +28,7 @@ def sim(blueprint, robots, material, minutes, mem):
 
   for i,costs in reversed(list(enumerate(blueprint))):
     # hack to reduce combinatorics
-    if i == 0 and minutes < 16+8:
-      continue
-    if i == 1 and minutes < 7+8:
+    if build_skipper(i,minutes):
       continue
 
     time = 1+max(timeToMine(m,c,r) for m,c,r in zip(material,costs,robots))
@@ -38,24 +36,30 @@ def sim(blueprint, robots, material, minutes, mem):
       continue
     new_robots = tuple(r+(1 if i==j else 0) for j,r in enumerate(robots))
     new_material = tuple(m+r*time-c for m,c,r in zip(material,costs,robots))
-    options.append(sim(blueprint, new_robots, new_material, minutes-time, mem))
+    options.append(sim(blueprint, new_robots, new_material, minutes-time, mem, build_skipper))
 
   result = max(options, key=lambda x: x[3])
   mem[fingerprint] = result
   return result
 
-#print(sum(sim(blueprint, (1,0,0,0), (0,0,0,0), 24, {})[3]*(i+1) for i,blueprint in enumerate(blueprints)))
+def build_skipper1(i, minutes):
+  return (i == 0 and minutes < 16) or (i == 1 and minutes < 7)
+
+#print(sum(sim(blueprint, (1,0,0,0), (0,0,0,0), 24, {}, build_skipper1)[3]*(i+1) for i,blueprint in enumerate(blueprints)))
 ss = 0
 for i,blueprint in enumerate(blueprints):
-  s = sim(blueprint, (1,0,0,0), (0,0,0,0), 24, {})
+  s = sim(blueprint, (1,0,0,0), (0,0,0,0), 24, {}, build_skipper1)
   print(i, blueprint, s)
   ss += s[3]*(i+1)
 print(ss)
 
-#print(math.prod(sim(blueprint, (1,0,0,0), (0,0,0,0), 32, {})[3] for blueprint in blueprints[:3]))
+def build_skipper2(i, minutes):
+  return (i == 0 and minutes < 24) or (i == 1 and minutes < 15)
+
+#print(math.prod(sim(blueprint, (1,0,0,0), (0,0,0,0), 32, {}, build_skipper2)[3] for blueprint in blueprints[:3]))
 ss = 1
 for i,blueprint in enumerate(blueprints[:3]):
-  s = sim(blueprint, (1,0,0,0), (0,0,0,0), 32, {})
+  s = sim(blueprint, (1,0,0,0), (0,0,0,0), 32, {}, build_skipper2)
   print(i, blueprint, s)
   ss *= s[3]
 print(ss)
