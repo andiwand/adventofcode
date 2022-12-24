88c6fc1fb7fd78201ad6ac0d35231f82cb118fdc
andiwand <stefl.andreas@gmail.com>
2022-12-24 16:02:33 +0100

    day 24 switch to bfs

--- 2022/day24/solution.py
+++ 2022/day24/solution.py
@@ -28,59 +28,29 @@ def wind_evolution(wind,dim,time):
 def wrap_wind_time(dim,time):
   return time % math.lcm(*[d-2 for d in dim])
 
-def print_world(world,wind,dim):
-  for y in range(0,dim[1]):
-    for x in range(0,dim[0]):
-      if (x,y) in world:
-        print('#', end='')
-      elif (x,y) in wind:
-        v = wind[(x,y)]
-        if isinstance(v,list):
-          if len(v) == 1:
-            v = v[0]
-          elif len(v) > 1:
-            v = len(v)
-          else:
-            raise RuntimeError('dafuq')
-        print(v, end='')
-      else:
-        print('.', end='')
-    print()
+def solve_bfs(world,world_dim,wind,start,finish):
+  def step(states,wind):
+    new_states = set()
+    for p,g in states:
+      for d in directions.values():
+        new_p = move(p,d)
+        if any(not 0 <= new_p[i] < world_dim[i] for i in range(len(start))):
+          continue
+        if new_p in world or new_p in wind:
+          continue
+        new_g = g + (1 if new_p == finish[g] else 0)
+        new_states.add((new_p,new_g))
+    return new_states
 
-def solve(world,world_dim,wind,start,finish,wind_time,time_left,mem):
-  if time_left <= 0:
-    return math.inf,0
-  if start == finish:
-    return 0,wind_time
-
-  fingerprint = (start,wind_time,time_left)
-  if fingerprint in mem:
-    return mem[fingerprint]
-
-  next_wind_time = wrap_wind_time(world_dim,wind_time+1)
-  next_wind = wind_evolution(wind,world_dim,next_wind_time)
-
-  options = [(math.inf,0)]
-  for d in directions.values():
-    new_p = move(start,d)
-    if any(not 0 <= new_p[i] < world_dim[i] for i in range(len(start))):
-      continue
-    if new_p in world or new_p in next_wind:
-      continue
-    options.append(solve(world,world_dim,wind,new_p,finish,next_wind_time,time_left-1,mem))
-
-  best = min(options,key=lambda o: o[0])
-  best = (1+best[0],best[1])
-  mem[fingerprint] = best
-  return mem[fingerprint]
-
-import sys
-sys.setrecursionlimit(10000)
+  states = [(start,0)]
+  time = 1
+  while True:
+    next_wind = wind_evolution(wind,world_dim,time)
+    states = step(states,next_wind)
+    if (finish[-1],len(finish)) in states:
+      return time
+    time += 1
 
 start,finish = (1,0),(world_dim[0]-2,world_dim[1]-1)
-s = solve(world,world_dim,wind,start,finish,0,350,{})
-print(s)
-s = solve(world,world_dim,wind,finish,start,s[1],350,{})
-print(s)
-s = solve(world,world_dim,wind,start,finish,s[1],350,{})
-print(s)
+print(solve_bfs(world,world_dim,wind,start,[finish]))
+print(solve_bfs(world,world_dim,wind,start,[finish,start,finish]))
